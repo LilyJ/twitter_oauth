2471a4afb8222cbf086677f3c14bb061ff3ada93
Lily <qvang.j@gmail.com>
2013-09-10 19:22:48 -0700

    user can now tweet on the app

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -18,9 +18,11 @@ get '/auth' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
+  
+  # session[:user] = @user
   @username = @access_token.params[:screen_name]
-  User.create(username: @username, oauth_token: @access_token.token, oauth_secret: @access_token.secret)
-
+  @user = User.create(username: @username, oauth_token: @access_token.token, oauth_secret: @access_token.secret)
+  session[:user_id] = @user.id
   session.delete(:request_token)
 
 
@@ -28,4 +30,16 @@ get '/auth' do
 
   erb :index
   
+end
+
+post '/auth/post' do
+
+  tweet = params[:tweet]
+  # p params
+  @user = User.find(session[:user_id])
+  # p @user
+  client = Twitter::Client.new(oauth_token: @user.oauth_token, oauth_token_secret: @user.oauth_secret)
+  client.update(tweet)
+  content_type :json
+  {tweet: tweet}.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('form').submit(function(event){
+    event.preventDefault();
+    var input = $('#input').val();
+    $.ajax({
+      url: this.action,
+      type: this.method,
+      data: {tweet: input}
+    }).done(function(response){
+      console.log("done")
+      $('#tweets_list').append('<li>' + response.tweet + '</li>')
+    });
+  })
 });
+
+
+
+ 
